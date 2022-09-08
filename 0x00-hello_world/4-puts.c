a46f8fa06b712ca890203853e6c737ab512300df
Chinazasunday <chinazasundaygoodness@gmail.com>
2022-09-08 16:34:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,13 @@
 
 /**
  * main - print the string in the function
- *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle") ;
-	return (0) ;
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
 		
