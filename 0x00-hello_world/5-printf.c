914974baa0e33938e9ca37b10faa0ffb04cbe757
Chinazasunday <chinazasundaygoodness@gmail.com>
2022-09-09 00:42:36 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return 0
+ * main- Entry point for the program.
+ *
+ * Return: 0 if no errors, return non zero if errors
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
