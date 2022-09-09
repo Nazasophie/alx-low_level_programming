8fd64073867cf541a144d95a5e4810a9a0600193
Chinazasunday <chinazasundaygoodness@gmail.com>
2022-09-09 02:06:14 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point for program.
  *
- *main -is the entry point for the program.
- *
- * Return:zero if no error, Non-zero value  if errors.
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: zero if no error, Non-zero value if errors.
  */
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-			printf("Size of an int: %ld byte(s)\n", sizeof(int));
-				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %ld byte(s)\n", sizeof(float));
-							return (0);
+	printf("Size of a char: %id byte(s)\n", sizeof(c));
+	printf("Size of an int: %id byte(s)\n", sizeof(i));
+	printf("Size of a long int: %id byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %id byte(s)\n", sizeof(lli));
+	printf("Size of a float: %id byte(s)\n", sizeof(f));
+	return (0);
 }
